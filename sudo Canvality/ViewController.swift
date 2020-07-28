97d29fffc39ecd8d76ab22db75b8d16110a624a0
Narek Stepanyan <narek.stepanyan@ramax.ru>
2020-07-28 20:39:34 +0300

    Приложение для администрирования Canvality

---
+++ sudo Canvality/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sudo Canvality
+//
+//  Created by Narek Stepanyan on 28.07.2020.
+//  Copyright © 2020 NRKK.DEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
